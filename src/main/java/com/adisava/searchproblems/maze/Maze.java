3e541b4889ca64ea5ca16c275cf619a400e83e5d
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-02-01 22:10:59 +0200

    Add random maze generation impl

--- src/main/java/com/adisava/searchproblems/maze/Maze.java
+++ src/main/java/com/adisava/searchproblems/maze/Maze.java
@@ -1,5 +1,7 @@
 package com.adisava.searchproblems.maze;
 
+import java.util.Arrays;
+
 public class Maze {
 
     public enum Cell {
@@ -19,5 +21,64 @@ public class Maze {
         public String toString() {
             return code;
         }
+    }
+
+    private final int rows, columns;
+    private final MazeLocation start, goal;
+    private Cell[][] grid;
+
+    public Maze(int rows, int columns, MazeLocation start, MazeLocation goal,
+                double sparseness) {
+
+        // initialize basic instance variables
+        this.rows = rows;
+        this.columns = columns;
+        this.start = start;
+        this.goal = goal;
+
+        // fill the grid with empty cells
+        grid = new Cell[rows][columns];
+
+        for (Cell[] row : grid) {
+            Arrays.fill(row, Cell.EMPTY);
+        }
+
+        // populate the grid with blocked cells
+        randomlyFill(sparseness);
+
+        // fill the start and goal locations
+        grid[start.row][start.column] = Cell.START;
+        grid[goal.row][goal.column] = Cell.GOAL;
+    }
+
+    public Maze() {
+        this(10, 10, new MazeLocation(0, 0), new MazeLocation(9, 9), 0.2);
+    }
+
+    private void randomlyFill(double sparseness) {
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (Math.random() < sparseness) {
+                    grid[row][column] = Cell.BLOCKED;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Cell[] row : grid) {
+            for (Cell cell : row) {
+                sb.append(cell.toString());
+            }
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Maze m = new Maze();
+        System.out.println(m);
     }
 }
